0713ef0e3fe5306d9f890ffc7dff449da4fc4152
Tiffany Sonny <tiffanysonny@gmail.com>
2023-01-09 21:03:37 -0800

    init

---
+++ src/test/java/io/typetowrite/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.typetowrite.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
